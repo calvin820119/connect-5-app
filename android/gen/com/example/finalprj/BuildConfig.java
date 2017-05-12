7d6a09a578c39a5cec564bcd585c2220df903d8b
Calvin Hsu <calvin820119@gmail.com>
2017-05-12 21:15:17 +0800

    update server android application source code \android

---
+++ android/gen/com/example/finalprj/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.finalprj;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- java project/bin/RunServer.bat
+++
@@ -1,4 +0,0 @@
-SET PATH=C:\Program Files\Java\jdk1.7.0_45\bin
-javac server.java
-java server
-pause
